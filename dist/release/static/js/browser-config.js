7efc8c14b45318797a1a4bbd8f8cdc71276ff165
Rocky Chen <Rocky.Chen@dpaw.wa.gov.au>
2017-06-14 08:39:06 +0800

    Configure supported browser.

---
+++ dist/release/static/js/browser-config.js
@@ -0,0 +1,18 @@
+var browser_config = { 
+    non_match_policy : "confirm",
+    non_match_timeout: 30,
+
+    browser_match_policy: "confirm",
+    browser_match_timeout:30,
+
+    full_match_timeout:604800,
+
+    requirements:{
+        firefox:">=48",
+        chrome:">=50",
+        chromium:">=50",
+        safari:">=9.1",
+        "mobile safari":">=5.0.2"
+    }   
+}
+
