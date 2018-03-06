7a13b7fe8b3fc83382a775aed239bc5b1f9da3a2
Rocky Chen <rocky.Chen@dbca.wa.gov.au>
2018-03-06 14:10:36 +0800

    Auto release the lock if the process who own the lock is dead; before timeout is for lock acquisition operation and not it is for lock itself.

--- gokart/file_lock.py
+++ gokart/file_lock.py
@@ -1,6 +1,9 @@
 import os
 import time
 import errno
+import json
+import traceback
+from datetime import datetime
  
 class FileLockException(Exception):
     pass
@@ -10,19 +13,79 @@ class FileLock(object):
         you can use it in a with statement. This should be relatively cross
         compatible as it doesn't rely on msvcrt or fcntl for the locking.
     """
- 
     def __init__(self, lock_file, timeout=10, delay=.05):
         """ Prepare the file locker. Specify the file to lock and optionally
             the maximum timeout and the delay between each attempt to lock.
         """
-        self.fd = None
+        self.acquired_lock_time = None
         self.lockfile = lock_file
         self.timeout = timeout
         self.delay = delay
 
     @property
     def is_locked(self):
-        return self.fd != None
+        if not self.acquired_lock_time:
+            #not locked
+            return False
+        elif time.time() - self.acquired_lock_time < self.timeout:
+            #acquired the lock and is not timeout
+            return True
+        elif self.acquired_lock_time != os.path.getmtime(self.lockfile):
+            #acquired the lock, but is already timeout, and lockfile is modified
+            self.acquired_lock_time = None
+            return False
+        else:
+            #acquired the lock, but is already timeout, and lockfile is not modified, renew the lock
+            os.utime(self.lockfile,None)
+            self.acquired_lock_time = os.path.getmtime(self.lockfile)
+            return True
+        
+
+    @property
+    def lock_metadata(self):
+        metadata = None
+        with open(self.lockfile,"r") as f:
+            metadata = f.read()
+        if metadata:
+            try:
+                metadata = json.loads(metadata)
+            except:
+                metadata = None
+        return metadata
+
+
+    @property
+    def is_locked_by_others(self):
+        if os.path.exists(self.lockfile):
+            locked = True
+            try:
+                metadata = self.lock_metadata
+                if metadata:
+                    if os.path.exists(os.path.join("/proc",str(metadata["pid"]))) and metadata["process_start_time"] == os.path.getmtime(os.path.join("/proc",str(metadata["pid"]),"cmdline")):
+                        #process exist and also the create time of the process is the same as the meta data
+                        locked = True
+                    else:
+                        locked = False
+            except:
+                traceback.print_exc()
+                pass
+
+            if locked:
+                #check timeout
+                if time.time() - os.path.getmtime(self.lockfile) >= self.timeout:
+                    if metadata:
+                        raise FileLockException("Lock is required by process ({}) at time ({}),and failed to release it within {} seconds".format(metadata["pid"],datetime.fromtimestamp(os.path.getmtime(self.lockfile)),self.timeout))
+                    else:
+                        raise FileLockException("Lock is required by other process, but failed to release it within {} seconds".format(self.timeout))
+            else:
+                try:
+                    os.remove(self.lockfile)
+                except:
+                    pass
+
+            return locked
+        else:
+            return False
  
     def waitUntilRelease(self):
         """ Wait until no one hold the lock, if possible. 
@@ -31,15 +94,8 @@ class FileLock(object):
             exceeds `timeout` number of seconds, in which case it throws 
             an exception.
         """
-        if self.is_locked:
-            self.release()
-        else:
-            start_time = time.time()
-            while os.path.exists(self.lockfile):
-                if (time.time() - start_time) >= self.timeout:
-                    raise FileLockException("Timeout occured.")
-                time.sleep(self.delay)
-
+        while self.is_locked_by_others:
+            time.sleep(self.delay)
  
     def acquire(self):
         """ Acquire the lock, if possible. If the lock is in use, it check again
@@ -51,28 +107,32 @@ class FileLock(object):
             #already get the lock
             if not os.path.exists(self.lockfile):
                 #the lock file doesn't exist for whatever reason
-                try:
-                    os.close(self.fd)
-                except:
-                    pass
-                self.fd = None
+                self.acquired_lock_time = None
 
         if self.is_locked:
             #already get the lock
             return
 
-        start_time = time.time()
         while True:
+            fd = None
             try:
-                self.fd = os.open(self.lockfile, os.O_CREAT|os.O_EXCL|os.O_RDWR)
+                fd = os.open(self.lockfile, os.O_CREAT|os.O_EXCL|os.O_RDWR)
+                os.write(fd,json.dumps({"pid":os.getpid(),"process_start_time":os.path.getmtime(os.path.join("/proc",str(os.getpid()),"cmdline"))}))
+                self.acquired_lock_time = os.path.getmtime(self.lockfile)
                 break;
             except OSError as e:
                 if e.errno != errno.EEXIST:
                     raise 
-                if (time.time() - start_time) >= self.timeout:
-                    raise FileLockException("Timeout occured.")
+                if not self.is_locked_by_others:
+                    #no one acquire the lock, try again
+                    continue
                 time.sleep(self.delay)
- 
+            finally:
+                if fd:
+                    try:
+                        os.close(fd)
+                    except:
+                        pass
  
     def release(self):
         """ Get rid of the lock by deleting the lockfile. 
@@ -81,15 +141,12 @@ class FileLock(object):
         """
         if self.is_locked:
             try:
-                os.close(self.fd)
+                if os.path.exists(self.lockfile):
+                    os.remove(self.lockfile)
             except:
                 pass
+            self.acquired_lock_time = None
 
-            if os.path.exists(self.lockfile):
-                os.remove(self.lockfile)
-
-            self.fd = None
- 
  
     def __enter__(self):
         """ Activated when used in the with statement. 
